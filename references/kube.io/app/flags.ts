5fa90cdc335d39d9eff1071eb13b904a91e3e33e
Yong Su <jean.timex@gmail.com>
2026-05-26 09:15:23 -0700

    removed reference files

--- references/kube.io/app/flags.ts
+++
@@ -1,5 +0,0 @@
-export const FLAGS = {
-  BLOG: true,
-  WORKSHOP: false,
-  HIRE_ME: false,
-};
--- references/kube.io/app/hooks/useIsInitialRender.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export function useIsInitialRender() {
-  const [isInitial, setIsInitial] = useState(true);
-  useEffect(() => {
-    setIsInitial(false);
-  }, []);
-  return isInitial;
-}
