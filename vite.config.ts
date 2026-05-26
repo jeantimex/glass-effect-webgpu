fa4d08859bcc1277ed35a7ac05b2fc437ced6ecf
Yong Su <jean.timex@gmail.com>
2026-05-26 09:17:53 -0700

    added github page support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/glass-effect-webgpu/' : '/',
   server: {
     port: 3000,
     open: true
   }
-})
+}))
